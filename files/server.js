ade2209ec7f3850e6a3be9026d94bc49f586b650
MasterIO02 <lilian.lobry@gmail.com>
2021-07-12 22:27:29 +0200

    Bump version

--- files/server.js
+++ files/server.js
@@ -1,7 +1,7 @@
 const io = require("socket.io-client")
 const dataProcessor = require("./dataProcessor")
 const config = require("../config.json")
-const version = 9
+const version = 10
 let ioClient
 
 var socketUrl
