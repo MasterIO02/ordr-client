a177387b9bd12332eaf78cd13bda8b834d66bf1e
MasterIO02 <lilian.lobry@gmail.com>
2021-11-03 15:37:25 +0100

    Update websocket example

--- ordr-socketio-example.js
+++ ordr-socketio-example.js
@@ -2,18 +2,18 @@ const socketUrl = "https://ordr-ws.issou.best"
 const io = require("socket.io-client")
 const ioClient = io.connect(socketUrl)
 
-ioClient.on("render_done", data => {
+ioClient.on("render_done_json", data => {
     console.log(data)
 })
 
-ioClient.on("render_progress", data => {
+ioClient.on("render_progress_json", data => {
     console.log(data)
 })
 
-ioClient.on("render_added", data => {
+ioClient.on("render_added_json", data => {
     console.log(data)
 })
 
-ioClient.on("render_failed", data => {
+ioClient.on("render_failed_json", data => {
     console.log(data)
 })
