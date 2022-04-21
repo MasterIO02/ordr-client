d61427042074460b401a387f0b752e431ba489de
MasterIO02 <lilian.lobry@gmail.com>
2022-04-21 16:56:17 +0200

    Bump version

--- files/server.js
+++ files/server.js
@@ -4,7 +4,7 @@ const dataProcessor = require("./dataProcessor")
 const config = require(process.cwd() + "/config.json")
 const { isRendering, abortRender } = require("./danserHandler")
 const { exit } = require("./util")
-const version = 16
+const version = 17
 let ioClient
 
 var socketUrl
