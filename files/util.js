2899bef87cb3eae8d0d8b269498c3e2d9f6ecccb
MasterIO02 <40836390+MasterIO02@users.noreply.github.com>
2023-02-22 16:43:41 +0100

    Refactor exit function

--- files/util.js
+++ files/util.js
@@ -2,11 +2,14 @@ const wget = require("wget-improved")
 const AdmZip = require("adm-zip")
 const fs = require("fs")
 
-exports.exit = () => {
-    return new Promise(() => {
+exports.exit = async () => {
+    await new Promise(resolve => {
         console.log("Press any key to exit.")
         process.stdin.setRawMode(true)
-        process.stdin.on("data", process.exit.bind(process, 0))
+        process.stdin.on("data", () => {
+            resolve()
+            process.exit(0)
+        })
     })
 }
 
@@ -14,7 +17,7 @@ exports.asyncDownload = async (link, output, filename, type) => {
     await new Promise((resolve, reject) => {
         let download = wget.download(link, output)
 
-        download.on("error", async (err) => {
+        download.on("error", async err => {
             console.log(err)
             await this.exit()
             reject()
@@ -32,14 +35,16 @@ exports.asyncDownload = async (link, output, filename, type) => {
 exports.asyncExtract = async (input, output, filename, type) => {
     await new Promise((resolve, reject) => {
         try {
-            const zip = new AdmZip(input);
-            zip.extractAllTo(output);
-            fs.unlinkSync(input);
-            console.log(`Finished unpacking ${type} ${filename}.`);
+            const zip = new AdmZip(input)
+            zip.extractAllTo(output)
+            fs.unlinkSync(input)
+            console.log(`Finished unpacking ${type} ${filename}.`)
             resolve()
         } catch (err) {
-            console.log("An error occured while unpacking the skin: " + err);
-            (async () => { this.exit() });
+            console.log("An error occured while unpacking the skin: " + err)
+            ;async () => {
+                this.exit()
+            }
             reject()
         }
     })
