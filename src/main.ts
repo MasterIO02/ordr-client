ba1e8299169dbf60960ecfff0c4ee9afbc01163c
MasterIO <40836390+MasterIO02@users.noreply.github.com>
2026-01-31 16:12:54 +0100

    Allow self-signed certificates if dev mode is enabled

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import fs from "fs"
 import { generateConfig, readConfig } from "./util/config"
 import injectTimestamp from "./util/inject_timestamp"
+import { setGlobalDispatcher, Agent } from "undici"
 
 async function main() {
     // generate a new config if there's none
@@ -11,6 +12,17 @@ async function main() {
     if (!config) {
         // we have an invalid config.json file, exiting the client (invalid values are logged by readConfig)
         process.exit(1)
+    }
+
+    // if we are in development mode, we can allow self-signed certificates
+    if (config.dev) {
+        setGlobalDispatcher(
+            new Agent({
+                connect: {
+                    rejectUnauthorized: false
+                }
+            })
+        )
     }
 
     if (config.log_timestamps) injectTimestamp()
